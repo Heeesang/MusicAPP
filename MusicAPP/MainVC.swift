19fd260acca8ea46bc597600672b2c4b414251b1
Heesang <ghs050611@naver.com>
2022-10-12 10:19:32 +0900

    :sparkles: ::

---
+++ MusicAPP/MainVC.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  MusicAPP
+//
+//  Created by 곽희상 on 2022/10/12.
+//
+
+import UIKit
+
+class MainVC: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
